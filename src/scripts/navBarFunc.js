762fc58c3053ccec1cb2f87b1c4ce1fc1b53560c
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-03-19 16:38:25 -0400

    nav bar styling in progress

--- src/scripts/navBarFunc.js
+++ src/scripts/navBarFunc.js
@@ -10,13 +10,34 @@ const scrollToPart = () => {
   const aboutUSSection = document.getElementById("about-us-container");
   const programsSection = document.getElementById("programs-container");
   const contactInfoSection = document.getElementById("contact-info-container");
+  const aboutUs = document.getElementById("selected-about-us");
+  const programs = document.getElementById("selected-programs");
+  const contactInfo = document.getElementById("selected-contact-info");
   navLinks.addEventListener("click", (e) => {
     if (e.target.classList.contains("about-us-option")) {
       aboutUSSection.scrollIntoView();
+      aboutUs.classList.add("selected");
+      if (programs.classList.contains("selected")) {
+        programs.classList.remove("selected");
+      } else if (contactInfo.classList.contains("selected")) {
+        contactInfo.classList.remove("selected");
+      }
     } else if (e.target.classList.contains("programs-option")) {
       programsSection.scrollIntoView();
+      programs.classList.add("selected");
+      if (aboutUs.classList.contains("selected")) {
+        aboutUs.classList.remove("selected");
+      } else if (contactInfo.classList.contains("selected")) {
+        contactInfo.classList.remove("selected");
+      }
     } else {
       contactInfoSection.scrollIntoView();
+      contactInfo.classList.add("selected");
+      if (aboutUs.classList.contains("selected")) {
+        aboutUs.classList.remove("selected");
+      } else if (programs.classList.contains("selected")) {
+        programs.classList.remove("selected");
+      }
     }
   });
 };
