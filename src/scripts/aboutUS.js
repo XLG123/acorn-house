62b3bf48b014ed0994660e35bdef0d2bfc9045fb
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-03-24 19:36:14 -0400

    automatic scrolling in progress, scrolling works once the page loads, but not after the page loads

--- src/scripts/aboutUS.js
+++ src/scripts/aboutUS.js
@@ -3,14 +3,34 @@ const automaticScrolling = () => {
   const imageOneBounding = document
     .getElementById("img1-container")
     .getBoundingClientRect();
-  // console.log(imageOneBounding);
 
+  const imageTwoBounding = document
+    .getElementById("img2-container")
+    .getBoundingClientRect();
+
+  const imageThreeBounding = document
+    .getElementById("img3-container")
+    .getBoundingClientRect();
+  
+  const width = imageOneBounding.width + 5;
+    
   // setInterval(() => {
+  //   console.log(imageTwoBounding);
+  //   console.log(imageThreeBounding);
   //   el.scrollBy({
-  //     left: imageOneBounding.width,
+  //     left: width,
   //     behavior: "smooth",
   //   });
-  // }, 3000);
+  // }, 5000);
+
+  // TODO:
+  // 1. When users has their mouse on the images, stop the automatic scrolling.
+  // 2. When a part of the image is hidden, scroll to the next image.
+  // 3. When it automatically scrolls to the third image, goes back to the first image after 5 seconds.
+
+  // el.addEventListener("mouseover", () => {
+
+  // })
 };
 
 export { automaticScrolling };
