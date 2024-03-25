78a7acea6bb11478a165230873ae5ca358345305
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-03-25 15:34:40 -0400

    automatic scrolling in progress: when scrolling, if part of the next image is showing in the viewport, then it will scroll to the next image

--- src/scripts/aboutUS.js
+++ src/scripts/aboutUS.js
@@ -1,30 +1,46 @@
 const automaticScrolling = () => {
   const el = document.getElementById("images");
-  const imageOne = document.getElementById("img1-container");
-  const imageTwo = document.getElementById("img2-container");
-  const imageThree = document.getElementById("img3-container");
+  const imgOne = document.getElementById("img1-container");
+  const imgTwo = document.getElementById("img2-container");
+  const imgThree = document.getElementById("img3-container");
 
-  let imageOneBounding,
-    imageTwoBounding,
-    imageThreeBounding = 0;
+  let imgOneBounding,
+    imgTwoBounding,
+    imgThreeBounding = 0;
 
-  let width = 0;
+  const imgCtnBounding = el.getBoundingClientRect();
 
   setInterval(() => {
-    imageOneBounding = imageOne.getBoundingClientRect();
-    imageTwoBounding = imageTwo.getBoundingClientRect();
-    imageThreeBounding = imageThree.getBoundingClientRect();
-    console.log(imageOneBounding, imageTwoBounding, imageThreeBounding);
-    el.scrollBy({
-      left: 542,
-      behavior: "smooth",
-    });
+    imgOneBounding = imgOne.getBoundingClientRect();
+    imgTwoBounding = imgTwo.getBoundingClientRect();
+    imgThreeBounding = imgThree.getBoundingClientRect();
+    if (
+      imgOneBounding.left < imgCtnBounding.left &&
+      imgOneBounding.right > imgCtnBounding.left
+    ) {
+      el.scrollBy({
+        left: imgTwoBounding.right - imgCtnBounding.right,
+        behavior: "smooth",
+      });
+    } else if (
+      imgTwoBounding.left < imgCtnBounding.left &&
+      imgTwoBounding.right > imgCtnBounding.left
+    ) {
+      el.scrollBy({
+        left: imgThreeBounding.right - imgCtnBounding.right,
+        behavior: "smooth",
+      });
+    } else {
+      el.scrollBy({
+        left: imgCtnBounding.width + 5,
+        behavior: "smooth",
+      });
+    }
   }, 5000);
 
   // TODO:
   // 1. When users has their mouse on the images, stop the automatic scrolling.
-  // 2. When a part of the image is hidden, scroll to the next image.
-  // 3. When it automatically scrolls to the third image, goes back to the first image after 5 seconds.
+  // 2. When it automatically scrolls to the third image, goes back to the first image after 5 seconds.
 
   // el.addEventListener("mouseover", () => {
 
