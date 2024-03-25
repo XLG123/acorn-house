a0a7902f7e78cd2db266145384681780f841c2df
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-03-25 16:05:17 -0400

    now when it scrolls to the third image, it will go back to the first image after 5 seconds

--- src/scripts/aboutUS.js
+++ src/scripts/aboutUS.js
@@ -30,6 +30,15 @@ const automaticScrolling = () => {
         left: imgThreeBounding.right - imgCtnBounding.right,
         behavior: "smooth",
       });
+    } else if (
+      imgThreeBounding.left === imgCtnBounding.left &&
+      imgThreeBounding.right === imgCtnBounding.right &&
+      imgOneBounding.left < 0
+    ) {
+      el.scrollBy({
+        left: imgOneBounding.left - imgThreeBounding.left,
+        behavior: "instant",
+      });
     } else {
       el.scrollBy({
         left: imgCtnBounding.width + 5,
@@ -40,7 +49,6 @@ const automaticScrolling = () => {
 
   // TODO:
   // 1. When users has their mouse on the images, stop the automatic scrolling.
-  // 2. When it automatically scrolls to the third image, goes back to the first image after 5 seconds.
 
   // el.addEventListener("mouseover", () => {
 
