1b23e6ad9fc375f54ea5f7891c454364c6c4d678
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-04-02 18:03:29 -0400

    testing js file for mobile view

--- src/scripts/contentLayoutResponsive.js
+++ src/scripts/contentLayoutResponsive.js
@@ -1,17 +1,19 @@
 import platform from 'platform';
 import { alignPrograms } from './programs';
 
+const cl = console.log.bind(console);
+
 const responsiveLayout = () => {
+
   alignPrograms();
   // TODO:
   // Modify the size of the content container for mobile/tablet view
   const contentCtn = document.getElementById("content-container");
-  if (platform.isMobile) {
+  cl(platform.toString());
+  cl(platform.name);
+  if (platform.name.includes("mobile") || platform.name.includes("Mobile")) {
     contentCtn.style.paddingLeft = "1em";
     contentCtn.style.paddingRight = "1em";
-    contentCtn.style.backgroundColor = "black";
-  } else {
-    contentCtn.style.backgroundColor = "Blue";
   }
 
 };
