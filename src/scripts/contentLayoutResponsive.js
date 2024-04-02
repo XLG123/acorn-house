8eeac53b135453083f49d391e5009a393ebf2d85
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-04-01 22:33:47 -0400

    Mobile View Content Layout in Progress

---
+++ src/scripts/contentLayoutResponsive.js
@@ -0,0 +1,12 @@
+const isMobileDevice = () => {
+  // TODO:
+  // Check if the device is a mobile device
+  // return true if it's mobile, false otherwise.
+};
+
+const responsiveLayout = () => {
+  // TODO:
+  // Modify the size of the content container for mobile/tablet view
+};
+
+export { responsiveLayout };
