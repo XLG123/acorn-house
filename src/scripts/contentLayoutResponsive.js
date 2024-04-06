77a8e58628c8c8b4cf18b69d0a7bd2183e7591ed
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-04-05 23:56:15 -0400

    testing responsive layout in mac os chrome

--- src/scripts/contentLayoutResponsive.js
+++ src/scripts/contentLayoutResponsive.js
@@ -1,22 +1,22 @@
-import platform from 'platform';
-import { alignPrograms } from './programs';
+import platform from "platform";
+import { alignPrograms } from "./programs";
 
 const cl = console.log.bind(console);
 
 const responsiveLayout = () => {
-
   alignPrograms();
   // TODO:
   // Modify the size of the content container for mobile/tablet view
   const contentCtn = document.getElementById("content-container");
-  if (platform.name.includes("mobile") || platform.name.includes("Mobile")) {
+  if (
+    !platform.os.family.includes("iOS") ||
+    !platform.os.family.includes("Android")
+  ) {
     contentCtn.style.paddingLeft = "1em";
     contentCtn.style.paddingRight = "1em";
-    cl(platform.toString());
-    cl(platform.name);
+    cl("this is not a touch device");
     contentCtn.style.backgroundColor = "red";
   }
-
 };
 
 export { responsiveLayout };
