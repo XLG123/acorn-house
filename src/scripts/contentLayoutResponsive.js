d50901f4f5ca13b74fefcc215b6230adbdd57877
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-04-12 15:50:36 -0400

    fixed minor bug on background position for programs title

--- src/scripts/contentLayoutResponsive.js
+++ src/scripts/contentLayoutResponsive.js
@@ -23,7 +23,7 @@ const removeTitleBgImg = () => {
   programsTitle.style.backgroundImage =
     'url("../src/assets/images/programs-paper.png"), url("../src/assets/images/acornBanner2.png")';
   programsTitle.style.backgroundRepeat = "no-repeat, space repeat";
-  programsTitle.style.position = "left 50% top 0.25em";
+  programsTitle.style.backgroundPosition = "left 50% top 0.25em";
   programsTitle.style.backgroundSize = "8em, 4em";
 };
 
