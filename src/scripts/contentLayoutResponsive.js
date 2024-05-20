f2ff8f6f84f5d631ae00c7ef995bfdc2d72d17b2
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-05-20 13:08:04 -0400

    fully converted animation.js into css stylings

--- src/scripts/contentLayoutResponsive.js
+++ src/scripts/contentLayoutResponsive.js
@@ -1,11 +1,9 @@
-import { disableAnimation } from "./animation";
 import { alignPrograms } from "./programs";
 
 // const cl = console.log.bind(console);
 
 const responsiveLayout = () => {
   alignPrograms();
-  disableAnimation();
 };
 
 export { responsiveLayout };
