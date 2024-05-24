9e867884c4c6a9be8c968527e6c937ebbcaf60b2
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-05-23 23:09:00 -0400

    subtitle container will have a margin added to the bottom if the Chinese subtitle wraps onto the next line

--- src/scripts/contentLayoutResponsive.js
+++ src/scripts/contentLayoutResponsive.js
@@ -1,9 +1,11 @@
+import { restructureSubtitle } from "./onlineContact";
 import { alignPrograms } from "./programs";
 
 // const cl = console.log.bind(console);
 
 const responsiveLayout = () => {
   alignPrograms();
+  restructureSubtitle();
 };
 
 export { responsiveLayout };
---
+++ src/scripts/onlineContact.js
@@ -0,0 +1,20 @@
+// Add a margin to the bottom of the container if the container's height changes, otherwise set it to 0px.
+const addMargin = (el, parentCtn) => {
+    if (el.offsetHeight !== 24) {
+        parentCtn.style.marginBottom = "1em";
+    } else {
+        parentCtn.style.marginBottom = "0px";
+    }
+};
+
+// Restructure the layout of the subtitle when screen size changes and the element is wrapping onto the next line.
+const restructureSubtitle = () => {
+    const subtitleCtn = document.getElementById("subtitle-content");
+    const chnSubtitle = subtitleCtn.children[1].children[1];
+    addMargin(chnSubtitle, subtitleCtn);
+    window.addEventListener("resize", () => {
+        addMargin(chnSubtitle, subtitleCtn);
+    })
+};
+
+export { restructureSubtitle };
