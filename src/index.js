2714e6594808243fc608502a93038aa97f3e1e40
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-08-02 17:09:13 -0400

    new styling added to navigation bar for screen sizes under 1351px

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import { reloadPage, scrollToPart } from "./scripts/navBarFunc";
+import {
+	reloadPage,
+	scrollToPart,
+	updateNavigationBar,
+} from "./scripts/navBarFunc";
 import { automaticScrolling } from "./scripts/aboutUS";
 import { modifyYear } from "./scripts/copyRight";
 import { responsiveLayout } from "./scripts/contentLayoutResponsive";
 
-document.addEventListener('DOMContentLoaded', () => {
-  reloadPage();
-  scrollToPart();
-  automaticScrolling();
-  responsiveLayout();
-  modifyYear();
-})
+document.addEventListener("DOMContentLoaded", () => {
+	reloadPage();
+	scrollToPart();
+	updateNavigationBar();
+	automaticScrolling();
+	responsiveLayout();
+	modifyYear();
+});
--- src/scripts/navBarFunc.js
+++ src/scripts/navBarFunc.js
@@ -1,44 +1,44 @@
 const reloadPage = () => {
-    const schoolName = document.getElementById("school-name-container");
-    schoolName.addEventListener("click", () => {
-        document.location.reload();
-    });
+	const schoolName = document.getElementById("school-name-container");
+	schoolName.addEventListener("click", () => {
+		document.location.reload();
+	});
 };
 
 const scrollToPart = () => {
-    const navLinks = document.getElementById("nav-links");
-    const aboutUSSection = document.getElementById("about-us-container");
-    const programsSection = document.getElementById("programs-container");
-    const contactInfoSection = document.getElementById(
-        "contact-info-container"
-    );
-    const aboutUs = document.getElementById("selected-about-us");
-    const programs = document.getElementById("selected-programs");
-    const contactInfo = document.getElementById("selected-contact-info");
+	const navLinks = document.getElementById("nav-links");
+	const aboutUSSection = document.getElementById("about-us-container");
+	const programsSection = document.getElementById("programs-container");
+	const contactInfoSection = document.getElementById(
+		"contact-info-container"
+	);
+	const aboutUs = document.getElementById("selected-about-us");
+	const programs = document.getElementById("selected-programs");
+	const contactInfo = document.getElementById("selected-contact-info");
 
-    navLinks.addEventListener("click", (e) => {
-        const target = e.target.classList;
-        if (target.contains("about-us-option")) {
-            aboutUSSection.scrollIntoView();
-            aboutUs.classList.add("selected");
-            if (programs.classList.contains("selected")) {
-                programs.classList.remove("selected");
-            } else if (contactInfo.classList.contains("selected")) {
-                contactInfo.classList.remove("selected");
-            }
-        } else if (target.contains("programs-option")) {
-            programsSection.scrollIntoView();
-            programs.classList.add("selected");
-            if (aboutUs.classList.contains("selected")) {
-                aboutUs.classList.remove("selected");
-            } else if (contactInfo.classList.contains("selected")) {
-                contactInfo.classList.remove("selected");
-            }
-        } else {
-            contactInfoSection.scrollIntoView();
-            contactInfo.classList.add("selected");
-        }
-    });
+	navLinks.addEventListener("click", (e) => {
+		const target = e.target.classList;
+		if (target.contains("about-us-option")) {
+			aboutUSSection.scrollIntoView();
+			aboutUs.classList.add("selected");
+			if (programs.classList.contains("selected")) {
+				programs.classList.remove("selected");
+			} else if (contactInfo.classList.contains("selected")) {
+				contactInfo.classList.remove("selected");
+			}
+		} else if (target.contains("programs-option")) {
+			programsSection.scrollIntoView();
+			programs.classList.add("selected");
+			if (aboutUs.classList.contains("selected")) {
+				aboutUs.classList.remove("selected");
+			} else if (contactInfo.classList.contains("selected")) {
+				contactInfo.classList.remove("selected");
+			}
+		} else {
+			contactInfoSection.scrollIntoView();
+			contactInfo.classList.add("selected");
+		}
+	});
 	removeSelectedStyle();
 };
 
@@ -54,17 +54,17 @@ const isFullyVisible = (container) => {
 
 // Remove the styling from the selected navigation item when it detects scrolling
 const removeSelectedStyle = () => {
-    const aboutUSSection = document.getElementById("about-us-container");
-    const programsSection = document.getElementById("programs-container");
-    const contactInfoSection = document.getElementById(
-        "contact-info-container"
-    );
+	const aboutUSSection = document.getElementById("about-us-container");
+	const programsSection = document.getElementById("programs-container");
+	const contactInfoSection = document.getElementById(
+		"contact-info-container"
+	);
 
 	const aboutUs = document.getElementById("selected-about-us");
 	const programs = document.getElementById("selected-programs");
 	const contactInfo = document.getElementById("selected-contact-info");
 
-    document.addEventListener("scroll", () => {
+	document.addEventListener("scroll", () => {
 		if (isFullyVisible(aboutUSSection)) {
 			aboutUs.classList.add("selected");
 			if (programs.classList.contains("selected")) {
@@ -94,7 +94,51 @@ const removeSelectedStyle = () => {
 			programs.classList.remove("selected");
 			contactInfo.classList.remove("selected");
 		}
-    });
+	});
 };
 
-export { reloadPage, scrollToPart };
+// Return true if the display style of the specific element is grid, otherwise return false.
+const isGrid = (el) => {
+	return window.getComputedStyle(el).getPropertyValue("display") === "grid";
+};
+
+// Remove the slash from the navigation link.
+const removeSlash = (navLinks) => {
+	for (let i = 0; i < navLinks.length; ++i) {
+		let link = navLinks[i].children[0].textContent;
+		if (link.endsWith("/")) {
+			link = link.substring(0, link.length - 1);
+			navLinks[i].children[0].textContent = link;
+		}
+	}
+};
+
+// Add slash to each of the navigation links.
+const addSlash = (navLinks) => {
+	for (let i = 0; i < navLinks.length; ++i) {
+		let link = navLinks[i].children[0].textContent;
+		if (!link.endsWith("/")) {
+			navLinks[i].children[0].textContent = link + "/";
+		}
+	}
+};
+
+// Update the name of the navigation links depends on the screen sizes.
+const updateNavigationItem = (navLinks) => {
+	if (window.innerWidth <= 1350 && isGrid(navLinks[0])) {
+		removeSlash(navLinks);
+	} else {
+		addSlash(navLinks);
+	}
+};
+
+// Update the navigation bar.
+const updateNavigationBar = () => {
+	const navLinks = document.getElementsByClassName("nav-option");
+	updateNavigationItem(navLinks);
+	window.addEventListener("resize", () => {
+		updateNavigationItem(navLinks);
+	});
+};
+
+export { reloadPage, scrollToPart, updateNavigationBar };
