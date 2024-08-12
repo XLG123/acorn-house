dd7457107323593b942cf5ee1f6449dc0f4f35f5
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-08-12 17:43:43 -0400

    reformatted code for index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import {
-	scrollToPart,
-	triggerReload,
-	updateNavigationBar
+    scrollToPart,
+    triggerReload,
+    updateNavigationBar
 } from "./scripts/navBarFunc";
 import { automaticScrolling } from "./scripts/aboutUS";
 import { modifyYear } from "./scripts/copyRight";
 import { responsiveLayout } from "./scripts/contentLayoutResponsive";
 
 document.addEventListener("DOMContentLoaded", () => {
-	triggerReload();
-	updateNavigationBar();
-	scrollToPart();
+    triggerReload();
+    updateNavigationBar();
+    scrollToPart();
     automaticScrolling();
     responsiveLayout();
     modifyYear();
