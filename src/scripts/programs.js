b0e969dc476aacb0fc96c95645f621fe08320650
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-10-03 18:52:30 -0400

    reducing unnecessary variable declarations

--- src/scripts/programs.js
+++ src/scripts/programs.js
@@ -5,7 +5,7 @@ const adjustMobileViewAlignment = (boundingOne, boundingTwo, boundingThree) => {
     } else {
         mobileView = false;
     }
-    
+
     const allPrograms = document.getElementsByClassName("programs");
     for (const program of allPrograms) {
         program.classList.toggle("mobile-view-available-programs", mobileView);
@@ -13,26 +13,19 @@ const adjustMobileViewAlignment = (boundingOne, boundingTwo, boundingThree) => {
 };
 
 const alignPrograms = () => {
-    const afterSchool = document.getElementById("after-school-container");
-    const enrichmentClasses = document.getElementById(
-        "enrichment-classes-container"
-    );
-    const languageLearning = document.getElementById(
-        "language-learning-container"
-    );
-
+    const allPrograms = document.getElementsByClassName("programs");
     let ctnOnePos, ctnTwoPos, ctnThreePos;
 
-    ctnOnePos = afterSchool.getBoundingClientRect().left;
-    ctnTwoPos = enrichmentClasses.getBoundingClientRect().left;
-    ctnThreePos = languageLearning.getBoundingClientRect().left;
+    ctnOnePos = allPrograms[0].getBoundingClientRect().left;
+    ctnTwoPos = allPrograms[1].getBoundingClientRect().left;
+    ctnThreePos = allPrograms[2].getBoundingClientRect().left;
 
     adjustMobileViewAlignment(ctnOnePos, ctnTwoPos, ctnThreePos);
 
     window.addEventListener("resize", () => {
-        ctnOnePos = afterSchool.getBoundingClientRect().left;
-        ctnTwoPos = enrichmentClasses.getBoundingClientRect().left;
-        ctnThreePos = languageLearning.getBoundingClientRect().left;
+        ctnOnePos = allPrograms[0].getBoundingClientRect().left;
+        ctnTwoPos = allPrograms[1].getBoundingClientRect().left;
+        ctnThreePos = allPrograms[2].getBoundingClientRect().left;
         adjustMobileViewAlignment(ctnOnePos, ctnTwoPos, ctnThreePos);
     });
 };
